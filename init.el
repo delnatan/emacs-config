9fe4df4bd0f0b50758a8597a7e79d2deaed25c5b
Daniel Elnatan <4480843+delnatan@users.noreply.github.com>
2024-03-05 10:12:22 -0800

    added init.el

---
+++ init.el
@@ -0,0 +1,40 @@
+  ;; install straight.el by bootstrapping
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+
+  (straight-use-package 'use-package)
+
+  (use-package straight
+    :custom
+    (straight-use-package-by-default t))
+
+  ;; org-mode version 9.6.15 came with emacs@29
+  (use-package org
+    :straight t)
+
+;; I moved my config file into an org file
+(org-babel-load-file "~/Apps/emacs-config/config.org")
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
